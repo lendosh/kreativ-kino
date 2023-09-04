525c04f89d620c67f90bccef425aee1fd6808f6c
lendosh <vladyslav.pomazanov@gmail.com>
2023-09-04 08:44:12 +0200

    Update temporary developing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import React from "react";
+import { GiAutoRepair } from 'react-icons/gi';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center p-24">
-      Site in developing
+    <main className="flex flex-col min-h-screen items-center justify-center p-24 gap-12">
+        <GiAutoRepair size={80}/>
+        Website in Entwicklung
     </main>
   )
 }
